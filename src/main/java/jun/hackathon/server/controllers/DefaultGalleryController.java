1aaf89fffcc99930983a94d2265b10e76c4820f0
Kidchai <o.naibova@gmail.com>
2023-11-11 17:27:37 -0600

    Move default images to root directory

--- src/main/java/jun/hackathon/server/controllers/DefaultGalleryController.java
+++ src/main/java/jun/hackathon/server/controllers/DefaultGalleryController.java
@@ -12,22 +12,18 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/api/default/gallery")
 public class DefaultGalleryController {
     @GetMapping("/image")
     public ResponseEntity<byte[]> getImage(@RequestParam("imageid") String imageId) throws IOException {
-        String resource = "/images/" + imageId + ".png";
-        InputStream in = getClass().getResourceAsStream(resource);
-        BufferedImage image = ImageIO.read(in);
-
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ImageIO.write(image, "png", baos);
-        byte[] imageBytes = baos.toByteArray();
-
+        String path = "images/" + imageId + ".png";
+        byte[] imageData = Files.readAllBytes(Paths.get(path));
         return ResponseEntity.ok()
                 .contentType(MediaType.IMAGE_JPEG)
-                .body(imageBytes);
+                .body(imageData);
     }
 }
